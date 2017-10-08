1d042cfc7bea6e4e5097415e38e4a0842e749fe5
ralph <ralph@ralph-Monza-T100>
2017-10-08 22:39:52 +0100

    Fixed relative path

--- includes/config.php
+++ includes/config.php
@@ -1,4 +1,4 @@
 <?php
-define('DB_PATH', 'sqlite:' . __DIR__ . '/database/myDB.sqlite');
+define('DB_PATH', 'sqlite:' . __DIR__ . '/../database/myDB.sqlite');
 define('DHT22_PIN', 17);
 define('LOGGER_ID', null);      // Used when moving Pi's around
