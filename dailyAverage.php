4e9a757ed56a909dfbbd1b7087d5280580ab5263
HughesR <ralph.hughes@cgg.com>
2017-11-28 11:01:29 +0000

    Added time period to daily average chart page

--- dailyAverage.php
+++ dailyAverage.php
@@ -3,9 +3,10 @@ require_once 'includes/config.php';
 require_once 'includes/database.php';
 require_once 'includes/util.php';
 
-$numDays = 90;
+$numDays = getNumDays();
 $seriesIndexes = getSelectedSeries();
 $seriesNames = getSeriesFromDB($Database);
+$timePeriods = array("1" => "1 Day", "2" => "2 Days", "4" => "4 Days", "7" => "1 Week", "14" => "2 Weeks", "30" => "1 Month", "42" => "6 Weeks", "91" => "3 Months", "183" => "6 Months", "274" => "9 Months", "365" => "1 Year", "548" => "18 Months", "731" => "2 Years");
 
 include 'includes/guiHeader.php';
 ?>
@@ -69,7 +70,9 @@ include 'includes/guiHeader.php';
 
     <div id="container" style="width: 100%; height: 80%;"></div>
     <form action="<?php echo $_SERVER['REQUEST_URI']; ?>" method="get" onsubmit="return validateForm()" name="theForm">
-
+<select name="numDays">
+                    <?= getTimePeriodOptionHTML($timePeriods, $numDays); ?>
+                </select>
     
     Select sensor(s):<br/>
                 <select name="seriesIndexes[]" multiple>
@@ -81,25 +84,14 @@ include 'includes/guiHeader.php';
 
 <?php
 
-
-function getSensorOptionHTML($seriesNames, $seriesIndexes) {
-    $output = "";
-    for ($i = 0; $i < count($seriesNames); $i++) {
-        if (in_array($i, $seriesIndexes)) {
-            $output = $output . "\t\t\t\t" . '<option selected value="' . $i . '">' . $seriesNames[$i] . "</option>\r\n";
-        } else {
-            $output = $output . "\t\t\t\t" . '<option value="' . $i . '">' . $seriesNames[$i] . "</option>\r\n";
-        }
+// Read page vars
+function getNumDays() {
+    if (!empty($_GET["numDays"])) {
+        $numDays = filter_input(INPUT_GET, 'numDays', FILTER_SANITIZE_NUMBER_INT);
+    } else {
+        $numDays = 2; // Default time period on first entering the page
     }
-    return $output;
-}
-
-function getSeriesFromDB($Database) {
-    $sql = "select distinct(sensor) from temps";
-    foreach ($Database->query($sql) as $row) {
-        $array[] = $row['sensor'];
-    }
-    return $array;
+    return $numDays;
 }
 
 function getSelectedSeries() {
@@ -121,6 +113,38 @@ function getSelectedSeries() {
     }
     return $seriesIndexes;
 }
+
+function getTimePeriodOptionHTML($timePeriods, $numDays) {
+    $output = "";
+    foreach ($timePeriods as $key => $value) {
+        if ($key == $numDays) {
+            $output = $output . "\t\t\t\t" . '<option selected value="' . $key . '">' . $value . "</option>\r\n";
+        } else {
+            $output = $output . "\t\t\t\t" . '<option value="' . $key . '">' . $value . "</option>\r\n";
+        }
+    }
+    return $output;
+}
+function getSensorOptionHTML($seriesNames, $seriesIndexes) {
+    $output = "";
+    for ($i = 0; $i < count($seriesNames); $i++) {
+        if (in_array($i, $seriesIndexes)) {
+            $output = $output . "\t\t\t\t" . '<option selected value="' . $i . '">' . $seriesNames[$i] . "</option>\r\n";
+        } else {
+            $output = $output . "\t\t\t\t" . '<option value="' . $i . '">' . $seriesNames[$i] . "</option>\r\n";
+        }
+    }
+    return $output;
+}
+
+function getSeriesFromDB($Database) {
+    $sql = "select distinct(sensor) from temps";
+    foreach ($Database->query($sql) as $row) {
+        $array[] = $row['sensor'];
+    }
+    return $array;
+}
+
 
 
 function fetchAllSeries($Database, $seriesIndexes, $seriesNames, $numDays) {
